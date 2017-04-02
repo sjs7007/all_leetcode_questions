20181f7edb47992d1d1f62feeb264194d940795c
Yask Srivastava <yasksrivastava@Yasks-MacBook-Pro.local>
2017-04-02 23:07:03 +0530

    updated

--- clean_links.py
+++ clean_links.py
@@ -1,20 +1,17 @@
-
-with open('question_links.txt') as f:
-  links =  f.read()
-
-
-links = links.split('\n')
-
-question_links = []
-for each in links:
-  if '/problems/' in each:
-    question_links.append(each)
-
-
 import requests
-
-
 from bs4 import BeautifulSoup
+
+
+def update_question_links(question_links):
+  with open('question_links.txt') as f:
+    links =  f.read()
+
+
+  links = links.split('\n')
+
+  for each in links:
+    if '/problems/' in each:
+      question_links.append(each)
 
 
 def get_question(question_link):
@@ -22,14 +19,22 @@ def get_question(question_link):
   resp = requests.get(question_link)
   soup = BeautifulSoup(resp.text, 'html.parser')
 
-  a =  soup.find_all('meta')[0].find_all('meta')
+  return soup.find_all('meta')[0].find_all('meta')[1]['content']
 
-  return  a[1]['content']
 
-for each_question_link in question_links:
-  try:
-    print '-----'
-    print get_question(each_question_link)
-    print '-----'
-  except:
-    continue
+
+def main():
+  question_links = []
+  update_question_links(question_links)
+  for each_question_link in question_links:
+    try:
+      print '-----'
+      print get_question(each_question_link)
+      print '-----'
+    except:
+      continue
+
+
+if __name__=='main':
+  main()
+  
